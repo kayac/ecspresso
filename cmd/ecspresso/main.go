42dff4ee2b4cc9fbe23116bfd603db5e7e9eed48
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2018-06-25 17:26:18 +0900

    fix typo.

--- cmd/ecspresso/main.go
+++ cmd/ecspresso/main.go
@@ -43,7 +43,7 @@ func _main() int {
 	rollback := kingpin.Command("rollback", "rollback service")
 	rollbackOption := ecspresso.RollbackOption{
 		DryRun: rollback.Flag("dry-run", "dry-run").Bool(),
-		DeregisterTaskDefinition: rollback.Flag("dereginster-task-definition", "deregister rolled back task definition").Bool(),
+		DeregisterTaskDefinition: rollback.Flag("deregister-task-definition", "deregister rolled back task definition").Bool(),
 	}
 
 	delete := kingpin.Command("delete", "delete service")
