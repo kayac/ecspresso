f51aba2498c55e333e1021a034a1a323c4946632
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-07-11 02:09:06 +0900

    isCodeDeploy

--- rollback.go
+++ rollback.go
@@ -17,7 +17,7 @@ func (d *App) Rollback(opt RollbackOption) error {
 		return errors.Wrap(err, "failed to describe service status")
 	}
 
-	if sv.DeploymentController != nil && *sv.DeploymentController.Type == "CODE_DEPLOY" {
+	if isCodeDeploy(sv) {
 		return errors.New("could not rollback service using deployment controller CODE_DEPLOY")
 	}
 
