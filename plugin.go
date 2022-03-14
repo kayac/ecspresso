5c4621c33e64a06ca5e2bd91968d9d8e7e888b10
mashiike <ikeda-masashi@kayac.com>
2022-03-14 10:34:21 +0900

    implement plugin alias with func_prefix

--- plugin.go
+++ plugin.go
@@ -3,9 +3,12 @@ package ecspresso
 import (
 	"errors"
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
+	"text/template"
 
+	"github.com/fatih/color"
 	"github.com/fujiwara/cfn-lookup/cfn"
 	"github.com/fujiwara/tfstate-lookup/tfstate"
 )
@@ -13,6 +16,7 @@ import (
 type ConfigPlugins []ConfigPlugin
 
 func (ps ConfigPlugins) Setup(c *Config) error {
+	ps.check()
 	for _, p := range ps {
 		if err := p.Setup(c); err != nil {
 			return err
@@ -21,9 +25,33 @@ func (ps ConfigPlugins) Setup(c *Config) error {
 	return nil
 }
 
+func (ps ConfigPlugins) check() {
+	indexesByID := make(map[string][]int, len(ps))
+	for i, p := range ps {
+		id := strings.Join([]string{
+			strings.ToLower(p.FuncPrefix),
+			strings.ToLower(p.Name),
+		}, ";")
+		indexesByID[id] = append(indexesByID[id], i)
+	}
+	for _, indexes := range indexesByID {
+		if len(indexes) > 1 {
+			i := indexes[0]
+			fmt.Fprintln(
+				os.Stderr,
+				color.YellowString(
+					"WARNING: plugin `%s` in %v are duplicates. Please specify `func_prefix` to avoid duplication.",
+					ps[i].Name, indexes,
+				),
+			)
+		}
+	}
+}
+
 type ConfigPlugin struct {
-	Name   string                 `yaml:"name"`
-	Config map[string]interface{} `yaml:"config"`
+	Name       string                 `yaml:"name"`
+	Config     map[string]interface{} `yaml:"config"`
+	FuncPrefix string                 `yaml:"func_prefix"`
 }
 
 func (p ConfigPlugin) Setup(c *Config) error {
@@ -35,6 +63,14 @@ func (p ConfigPlugin) Setup(c *Config) error {
 	default:
 		return fmt.Errorf("plugin %s is not available", p.Name)
 	}
+}
+
+func (p ConfigPlugin) ModifyFuncMap(funcMap template.FuncMap) template.FuncMap {
+	modified := make(template.FuncMap, len(funcMap))
+	for funcName, f := range funcMap {
+		modified[strings.ToLower(p.FuncPrefix)+funcName] = f
+	}
+	return modified
 }
 
 func setupPluginTFState(p ConfigPlugin, c *Config) error {
@@ -61,6 +97,7 @@ func setupPluginTFState(p ConfigPlugin, c *Config) error {
 	if err != nil {
 		return err
 	}
+	funcs = p.ModifyFuncMap(funcs)
 	c.templateFuncs = append(c.templateFuncs, funcs)
 	return nil
 }
@@ -70,6 +107,7 @@ func setupPluginCFn(p ConfigPlugin, c *Config) error {
 	if err != nil {
 		return err
 	}
+	funcs = p.ModifyFuncMap(funcs)
 	c.templateFuncs = append(c.templateFuncs, funcs)
 	return nil
 }
