f3ef01914fbb36134699fe45c2da7fc7ff80e73e
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-08-20 00:20:55 +0900

    switch to use github.com/fujiwara/ecsta

--- exec.go
+++ exec.go
@@ -1,31 +1,12 @@
 package ecspresso
 
 import (
-	"bufio"
-	"bytes"
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"os/signal"
-	"sort"
-	"strconv"
-	"strings"
 
-	"github.com/Songmu/prompter"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ecs"
-	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/fujiwara/ecsta"
 	"github.com/pkg/errors"
 )
-
-const SessionManagerPluginBinary = "session-manager-plugin"
-
-type taskFinderOption interface {
-	taskID() string
-}
 
 type ExecOption struct {
 	ID        *string
@@ -37,246 +18,32 @@ type ExecOption struct {
 	Port        *int
 }
 
-func (o ExecOption) taskID() string {
-	return aws.StringValue(o.ID)
+func (d *App) NewEcsta(ctx context.Context) (*ecsta.Ecsta, error) {
+	return ecsta.New(ctx, d.config.Region, d.config.Cluster)
 }
 
 func (d *App) Exec(opt ExecOption) error {
 	ctx, cancel := d.Start()
 	defer cancel()
 
-	if _, err := exec.LookPath(SessionManagerPluginBinary); err != nil {
-		return errors.Wrapf(err, "%s is not installed.\nSee also https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html\n", SessionManagerPluginBinary)
-	}
-
-	// find a task to exec
-	tasks, err := d.listTasks(ctx, opt.ID, "RUNNING")
+	ecstaApp, err := d.NewEcsta(ctx)
 	if err != nil {
-		return err
-	}
-	if len(tasks) == 0 {
-		d.Log("tasks not found")
-		return nil
-	}
-
-	task, err := d.findTask(opt, tasks)
-	if err != nil {
-		return err
-	}
-
-	// find a container to exec
-	var targetContainer *string
-	if len(task.Containers) == 1 {
-		targetContainer = task.Containers[0].Name
-	} else if aws.StringValue(opt.Container) != "" {
-		targetContainer = opt.Container
-	} else {
-		// select a container to execute
-		buf := new(bytes.Buffer)
-		sort.SliceStable(task.Containers, func(i, j int) bool {
-			return aws.StringValue(task.Containers[i].Name) < aws.StringValue(task.Containers[j].Name)
-		})
-		for _, container := range task.Containers {
-			fmt.Fprintln(buf, string(*container.Name))
-		}
-		result, err := d.runFilter(buf, "container name")
-		if err != nil {
-			return errors.Wrap(err, "failed to execute filter")
-		}
-		targetContainer = aws.String(strings.Fields(string(result))[0])
+		return errors.Wrap(err, "failed to create ecsta app")
 	}
 
 	if aws.BoolValue(opt.PortForward) {
-		return d.portForward(ctx, task, targetContainer, *opt.LocalPort, *opt.Port)
-	}
-
-	out, err := d.ecs.ExecuteCommand(&ecs.ExecuteCommandInput{
-		Cluster:     task.ClusterArn,
-		Interactive: aws.Bool(true),
-		Task:        task.TaskArn,
-		Command:     opt.Command,
-		Container:   targetContainer,
-	})
-	if err != nil {
-		return errors.Wrap(err, "failed to execute command. See also https://github.com/aws-containers/amazon-ecs-exec-checker")
-	}
-	sess, _ := json.Marshal(out.Session)
-	ssmReq, err := d.buildSsmRequestParameters(task, targetContainer)
-	if err != nil {
-		return errors.Wrap(err, "failed to build ssm request parameters")
-	}
-
-	cmd := exec.Command(
-		SessionManagerPluginBinary,
-		string(sess),
-		d.config.Region,
-		"StartSession",
-		"",
-		ssmReq.String(),
-		d.ecs.Endpoint,
-	)
-	signal.Ignore(os.Interrupt)
-	defer signal.Reset(os.Interrupt)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-func (d *App) runFilter(src io.Reader, title string) (string, error) {
-	command := d.config.FilterCommand
-	if command == "" {
-		return runInternalFilter(src, title)
-	}
-	var f *exec.Cmd
-	if strings.Contains(command, " ") {
-		f = exec.Command("sh", "-c", command)
+		return ecstaApp.RunPortforward(ctx, &ecsta.PortforwardOption{
+			ID:         aws.StringValue(opt.ID),
+			Container:  aws.StringValue(opt.Container),
+			LocalPort:  aws.IntValue(opt.LocalPort),
+			RemotePort: aws.IntValue(opt.Port),
+			RemoteHost: "", // TODO
+		})
 	} else {
-		f = exec.Command(command)
+		return ecstaApp.RunExec(ctx, &ecsta.ExecOption{
+			ID:        aws.StringValue(opt.ID),
+			Command:   aws.StringValue(opt.Command),
+			Container: aws.StringValue(opt.Container),
+		})
 	}
-	f.Stderr = os.Stderr
-	p, _ := f.StdinPipe()
-	go func() {
-		io.Copy(p, src)
-		p.Close()
-	}()
-	b, err := f.Output()
-	if err != nil {
-		return "", errors.Wrap(err, "failed to execute filter command")
-	}
-	return string(b), nil
-}
-
-/* buildSsmRequestParameters and getContainerRuntimeID are ported from aws-cli.
-https://github.com/aws/aws-cli/blob/054e0f194cfe9cf3642994b583fe438c56a97dfc/awscli/customizations/ecs/executecommand.py#L45
-
-# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-*/
-
-type ssmRequestParameters struct {
-	Target string
-}
-
-func (p *ssmRequestParameters) String() string {
-	b, _ := json.Marshal(p)
-	return string(b)
-}
-
-func (d *App) buildSsmRequestParameters(task *ecs.Task, targetContainer *string) (*ssmRequestParameters, error) {
-	values := strings.Split(*task.TaskArn, "/")
-	clusterName := values[1]
-	taskID := values[2]
-	runtimeID, err := d.getContainerRuntimeID(task, targetContainer)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get container runtime ID")
-	}
-	return &ssmRequestParameters{
-		Target: fmt.Sprintf("ecs:%s_%s_%s", clusterName, taskID, *runtimeID),
-	}, nil
-}
-
-func (d *App) getContainerRuntimeID(task *ecs.Task, targetContainer *string) (*string, error) {
-	output, err := d.ecs.DescribeTasks(&ecs.DescribeTasksInput{
-		Cluster: task.ClusterArn,
-		Tasks:   []*string{task.TaskArn},
-	})
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to describe tasks")
-	}
-	for _, t := range output.Tasks {
-		for _, c := range t.Containers {
-			if aws.StringValue(c.Name) == aws.StringValue(targetContainer) {
-				return c.RuntimeId, nil
-			}
-		}
-	}
-	return nil, errors.Errorf("container %s is not found in task %s", *targetContainer, *task.TaskArn)
-}
-
-func runInternalFilter(src io.Reader, title string) (string, error) {
-	var items []string
-	s := bufio.NewScanner(src)
-	for s.Scan() {
-		fmt.Println(s.Text())
-		items = append(items, strings.Fields(s.Text())[0])
-	}
-
-	var input string
-	for {
-		input = prompter.Prompt("Enter "+title, "")
-		if input == "" {
-			continue
-		}
-		var found []string
-		for _, item := range items {
-			item := item
-			if item == input {
-				found = []string{item}
-				break
-			} else if strings.HasPrefix(item, input) {
-				found = append(found, item)
-			}
-		}
-
-		switch len(found) {
-		case 0:
-			fmt.Printf("no such item %s\n", input)
-		case 1:
-			fmt.Printf("%s=%s\n", title, found[0])
-			return found[0], nil
-		default:
-			fmt.Printf("%s is ambiguous\n", input)
-		}
-	}
-}
-
-func (d *App) portForward(ctx context.Context, task *ecs.Task, targetContainer *string, localPort, remotePort int) error {
-	if remotePort == 0 {
-		return fmt.Errorf("--port is required")
-	}
-
-	ssmclient := ssm.New(d.sess)
-	ssmReq, err := d.buildSsmRequestParameters(task, targetContainer)
-	if err != nil {
-		return err
-	}
-	res, err := ssmclient.StartSession(&ssm.StartSessionInput{
-		Target:       aws.String(ssmReq.Target),
-		DocumentName: aws.String("AWS-StartPortForwardingSession"),
-		Parameters: map[string][]*string{
-			"portNumber":      {aws.String(strconv.Itoa(remotePort))},
-			"localPortNumber": {aws.String(strconv.Itoa(localPort))},
-		},
-		Reason: aws.String("port forwarding by ecspresso"),
-	})
-	if err != nil {
-		return errors.Wrap(err, "failed to start SSM session")
-	}
-	ssmSess, _ := json.Marshal(res)
-	d.DebugLog(SessionManagerPluginBinary, string(ssmSess), d.config.Region, "StartSession", "", ssmReq.String(), d.ecs.Endpoint)
-
-	cmd := exec.Command(
-		SessionManagerPluginBinary,
-		string(ssmSess),
-		d.config.Region,
-		"StartSession",
-		"",
-		ssmReq.String(),
-		d.ecs.Endpoint,
-	)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
 }
--- tasks.go
+++ tasks.go
@@ -1,19 +1,12 @@
 package ecspresso
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"io"
-	"os"
-	"strings"
 	"time"
 
-	"github.com/Songmu/prompter"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecs"
-	"github.com/fujiwara/tracer"
-	"github.com/olekukonko/tablewriter"
+	"github.com/fujiwara/ecsta"
 	"github.com/pkg/errors"
 )
 
@@ -30,14 +23,62 @@ func (o TasksOption) taskID() string {
 	return aws.StringValue(o.ID)
 }
 
-func (opt TasksOption) newFormatter() taskFormatter {
-	switch *opt.Output {
-	case "json":
-		return newTaskFormatterJSON(os.Stdout)
-	case "tsv":
-		return newTaskFormatterTSV(os.Stdout, true)
+func (d *App) Tasks(opt TasksOption) error {
+	ctx, cancel := d.Start()
+	defer cancel()
+
+	ecstaApp, err := d.NewEcsta(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create ecsta")
 	}
-	return newTaskFormatterTable(os.Stdout)
+	ecstaApp.Config.Set("output", aws.StringValue(opt.Output))
+
+	if aws.BoolValue(opt.Find) {
+		return ecstaApp.RunDescribe(ctx, &ecsta.DescribeOption{
+			ID: opt.taskID(),
+		})
+	} else if aws.BoolValue(opt.Stop) {
+		return ecstaApp.RunStop(ctx, &ecsta.StopOption{
+			ID:    opt.taskID(),
+			Force: aws.BoolValue(opt.Force),
+		})
+	} else if aws.BoolValue(opt.Trace) {
+		return ecstaApp.RunTrace(ctx, &ecsta.TraceOption{
+			ID:       opt.taskID(),
+			Duration: time.Minute,
+		})
+	} else {
+		family, err := d.taskDefinitionFamily(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to get task definition family")
+		}
+		return ecstaApp.RunList(ctx, &ecsta.ListOption{
+			Family: family,
+		})
+	}
+}
+
+func (d *App) taskDefinitionFamily(ctx context.Context) (string, error) {
+	var family string
+	if d.config.Service != "" {
+		sv, err := d.DescribeService(ctx)
+		if err != nil {
+			return "", err
+		}
+		tdArn := sv.TaskDefinition
+		td, err := d.DescribeTaskDefinition(ctx, *tdArn)
+		if err != nil {
+			return "", err
+		}
+		family = aws.StringValue(td.Family)
+	} else {
+		td, err := d.LoadTaskDefinition(d.config.TaskDefinitionPath)
+		if err != nil {
+			return "", err
+		}
+		family = aws.StringValue(td.Family)
+	}
+	return family, nil
 }
 
 func (d *App) listTasks(ctx context.Context, id *string, desiredStatuses ...string) ([]*ecs.Task, error) {
@@ -113,177 +154,4 @@ func (d *App) listTasks(ctx context.Context, id *string, desiredStatuses ...stri
 		}
 	}
 	return tasks, nil
-}
-
-func (d *App) Tasks(opt TasksOption) error {
-	ctx, cancel := d.Start()
-	defer cancel()
-
-	tasks, err := d.listTasks(ctx, opt.ID)
-	if err != nil {
-		return err
-	}
-	if len(tasks) == 0 {
-		d.Log("tasks not found")
-		return nil
-	}
-
-	if !aws.BoolValue(opt.Find) && !aws.BoolValue(opt.Stop) && !aws.BoolValue(opt.Trace) {
-		formatter := opt.newFormatter()
-		for _, task := range tasks {
-			formatter.AddTask(task)
-		}
-		formatter.Close()
-		return nil
-	}
-
-	task, err := d.findTask(opt, tasks)
-	if err != nil {
-		return err
-	}
-
-	if aws.BoolValue(opt.Find) {
-		f := newTaskFormatterJSON(os.Stdout)
-		f.AddTask(task)
-		f.Close()
-		return nil
-	} else if aws.BoolValue(opt.Stop) {
-		stop := aws.BoolValue(opt.Force) ||
-			prompter.YN(fmt.Sprintf("Stop task %s?", arnToName(*task.TaskArn)), false)
-		if !stop {
-			return nil
-		}
-		d.Log("Request stop task ID " + arnToName(*task.TaskArn))
-		_, err := d.ecs.StopTask(&ecs.StopTaskInput{
-			Cluster: task.ClusterArn,
-			Task:    task.TaskArn,
-			Reason:  aws.String("Request stop task by user action."),
-		})
-		if err != nil {
-			return errors.Wrap(err, "failed to stop task")
-		}
-	} else if aws.BoolValue(opt.Trace) {
-		tr, err := tracer.New(ctx, d.ecs, d.cwl)
-		if err != nil {
-			return errors.Wrap(err, "failed to new tracer")
-		}
-		if err := tr.Run(*task.ClusterArn, *task.TaskArn); err != nil {
-			return errors.Wrap(err, "failed to run tracer")
-		}
-	}
-
-	return nil
-}
-
-func (d *App) findTask(opt taskFinderOption, tasks []*ecs.Task) (*ecs.Task, error) {
-	if len(tasks) == 1 && opt.taskID() == arnToName(*tasks[0].TaskArn) {
-		return tasks[0], nil
-	}
-	buf := new(bytes.Buffer)
-	tasksDict := make(map[string]*ecs.Task)
-	formatter := newTaskFormatterTSV(buf, false)
-	for _, task := range tasks {
-		task := task
-		formatter.AddTask(task)
-		tasksDict[arnToName(*task.TaskArn)] = task
-	}
-	formatter.Close()
-	result, err := d.runFilter(buf, "task ID")
-	if err != nil {
-		return nil, err
-	}
-	ts := strings.Fields(string(result))
-	if len(ts) == 0 || ts[0] == "" {
-		return nil, errors.New("no any selected task")
-	}
-	taskID := ts[0]
-	if task, found := tasksDict[taskID]; found {
-		return task, nil
-	}
-	return nil, errors.Errorf("task ID %s is not found", taskID)
-}
-
-type taskFormatter interface {
-	AddTask(*ecs.Task)
-	Close()
-}
-
-var taskFormatterColumns = []string{
-	"ID",
-	"TaskDefinition",
-	"Instance",
-	"LastStatus",
-	"DesiredStatus",
-	"CreatedAt",
-	"Group",
-	"Type",
-}
-
-func taskToColumns(task *ecs.Task) []string {
-	return []string{
-		arnToName(*task.TaskArn),
-		arnToName(*task.TaskDefinitionArn),
-		arnToName(aws.StringValue(task.ContainerInstanceArn)),
-		aws.StringValue(task.LastStatus),
-		aws.StringValue(task.DesiredStatus),
-		task.CreatedAt.In(time.Local).Format(time.RFC3339),
-		aws.StringValue(task.Group),
-		aws.StringValue(task.LaunchType),
-	}
-}
-
-type taskFormatterTable struct {
-	table *tablewriter.Table
-}
-
-func newTaskFormatterTable(w io.Writer) *taskFormatterTable {
-	t := &taskFormatterTable{
-		table: tablewriter.NewWriter(w),
-	}
-	t.table.SetHeader(taskFormatterColumns)
-	t.table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-	return t
-}
-
-func (t *taskFormatterTable) AddTask(task *ecs.Task) {
-	t.table.Append(taskToColumns(task))
-}
-
-func (t *taskFormatterTable) Close() {
-	t.table.Render()
-}
-
-type taskFormatterTSV struct {
-	w io.Writer
-}
-
-func newTaskFormatterTSV(w io.Writer, header bool) *taskFormatterTSV {
-	t := &taskFormatterTSV{w: w}
-	if header {
-		fmt.Fprintln(t.w, strings.Join(taskFormatterColumns, "\t"))
-	}
-	return t
-}
-
-func (t *taskFormatterTSV) AddTask(task *ecs.Task) {
-	fmt.Fprintln(t.w, strings.Join(taskToColumns(task), "\t"))
-}
-
-func (t *taskFormatterTSV) Close() {
-}
-
-type taskFormatterJSON struct {
-	w io.Writer
-}
-
-func newTaskFormatterJSON(w io.Writer) *taskFormatterJSON {
-	return &taskFormatterJSON{w: w}
-}
-
-func (t *taskFormatterJSON) AddTask(task *ecs.Task) {
-	b, _ := marshalJSON(task)
-	t.w.Write(b.Bytes())
-}
-
-func (t *taskFormatterJSON) Close() {
 }
