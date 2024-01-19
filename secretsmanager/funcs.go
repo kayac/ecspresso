7b92146b5253189e3a9965b85a060bce92d1d06e
fujiwara <fujiwara.shunichiro@gmail.com>
2024-01-19 23:33:51 +0900

    Add caching for secretsmanager_arn function

--- secretsmanager/funcs.go
+++ secretsmanager/funcs.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"html/template"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
@@ -11,15 +12,21 @@ import (
 
 func FuncMap(ctx context.Context, cfg aws.Config) (template.FuncMap, error) {
 	smsvc := secretsmanager.NewFromConfig(cfg)
+	cache := sync.Map{}
 	funcs := template.FuncMap{
 		"secretsmanager_arn": func(id string) (string, error) {
+			if arn, ok := cache.Load(id); ok {
+				return arn.(string), nil
+			}
 			res, err := smsvc.DescribeSecret(ctx, &secretsmanager.DescribeSecretInput{
 				SecretId: &id,
 			})
 			if err != nil {
 				return "", fmt.Errorf("failed to describe secret: %w", err)
 			}
-			return *res.ARN, nil
+			arn := aws.ToString(res.ARN)
+			cache.Store(id, arn)
+			return arn, nil
 		},
 	}
 	return funcs, nil
